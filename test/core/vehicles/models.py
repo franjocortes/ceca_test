78966ee83880b1b8a39f8049ec9fc1447caafb97
franjo <franjo.cortes@gmail.com>
2022-02-02 12:17:26 -0300

    add block js in base.html and change fields name in models.py

--- test/core/vehicles/models.py
+++ test/core/vehicles/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Vehicle(models.Model):
-    name = models.CharField(max_length=300)
-    plate = models.CharField(max_length=10)
+    car_name = models.CharField(max_length=300)
+    car_plate = models.CharField(max_length=10)
 
     def __str__(self):
-        return f'{self.plate} - {self.name}'
+        return f'{self.car_plate} - {self.car_name}'
